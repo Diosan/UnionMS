1f45219af9d29d913368c8d24d180d1c9280529d
Dion Santana <dion.santana@gmail.com>
2013-06-08 16:46:08 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnionMS::Application.config.secret_token = 'bb10af7f4e8c90122152095a2bbb4b4e34b1ccbbd86c734b3aad2e539d476ad5016388dc4871b2ca561aeba5df2cd424f5331c7ef141c3b1da8e6515c7919d96'
